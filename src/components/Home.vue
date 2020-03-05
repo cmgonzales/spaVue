8c1cc4746fa01f8fbc95c78b98419bc5e379e76c
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-03-04 19:15:02 -0800

    second commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
         <h1>The Home Page</h1>
         <hr>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur
-            dolore .</p>
+            .</p>
 
     </div>
 </template>
