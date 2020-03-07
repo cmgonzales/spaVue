0471d0e493ab63a49ed49c0760e97828ce19ea15
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-03-06 23:23:23 -0800

    added action

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>The Home Page</h1>
         <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur
+        <p>Loremum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur
             .</p>
 
     </div>
